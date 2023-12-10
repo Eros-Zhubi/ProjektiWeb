35a9a380547f347f5217f630805b1c788f7cdbab
Eros-Zhubi <eroszhubi12@gmail.com>
2023-12-10 19:18:25 +0100

    Home finished

--- slideshow.js
+++ slideshow.js
@@ -16,7 +16,7 @@ function showSlides() {
 
     slides[slideIndex - 1].style.display = "block";
 
-    setTimeout(showSlides, 10000); // Change image every 5 seconds
+    setTimeout(showSlides, 10000); // Change image every 10 seconds
 }
 
 showSlides();
